9ecb40e941d0284f0cc36e2f8b52571ff3ff78f9
Sean Bohuslavsky <seanboh22@gmail.com>
2026-02-19 17:57:44 -0700

    zigbee

---
+++ zigbee2mqtt/usb.sh
@@ -0,0 +1,11 @@
+# From https://smarthomecircle.com/setup-home-assistant-connect-zbt-2-home-assistant-zigbee2mqtt-docker
+for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
+    (
+        syspath="${sysdevpath%/dev}"
+        devname="$(udevadm info -q name -p $syspath)"
+        [[ "$devname" == "bus/"* ]] && exit
+        eval "$(udevadm info -q property --export -p $syspath)"
+        [[ -z "$ID_SERIAL" ]] && exit
+        echo "/dev/$devname - $ID_SERIAL"
+    )
+done
